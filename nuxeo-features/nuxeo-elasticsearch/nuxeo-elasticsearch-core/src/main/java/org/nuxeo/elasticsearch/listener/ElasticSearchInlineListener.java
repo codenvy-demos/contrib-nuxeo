sydisnet/nuxeo

NXP-16212: refactoring indexation, removing post commit listener

nuxeo-features/nuxeo-elasticsearch/nuxeo-elasticsearch-core/src/main/java/org/nuxeo/elasticsearch/listener/ElasticSearchInlineListener.java
@@ -17,6 +17,7 @@
 
 package org.nuxeo.elasticsearch.listener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -46,17 +47,14 @@ public class ElasticSearchInlineListener extends IndexingCommandsStacker impleme
 
     private static final Log log = LogFactory.getLog(ElasticSearchInlineListener.class);
 
-    // rely on TransactionManager rather than on Save event
-    protected static boolean useTxSync = true;
-
     protected static ThreadLocal<Map<String, IndexingCommands>> transactionCommands = new ThreadLocal<Map<String, IndexingCommands>>() {
         @Override
         protected HashMap<String, IndexingCommands> initialValue() {
-            return new HashMap<String, IndexingCommands>();
+            return new HashMap<>();
         }
     };
 
-    protected static ThreadLocal<Boolean> synched = new ThreadLocal<Boolean>() {
+    protected static ThreadLocal<Boolean> isEnlisted = new ThreadLocal<Boolean>() {
         @Override
         protected Boolean initialValue() {
             return Boolean.FALSE;
@@ -70,105 +68,87 @@ public class ElasticSearchInlineListener extends IndexingCommandsStacker impleme
 
     @Override
     public void handleEvent(Event event) throws ClientException {
-
         String eventId = event.getName();
-
-        // manual flush on save if TxManager is not hooked
-        if (event.isCommitEvent()) {
-            if (!synched.get()) {
-                flushCommands();
+        if (!isEnlisted.get()) {
+            if (event.isCommitEvent()) {
+                // manual flush on save if TxManager is not hooked
+                afterCompletion(Status.STATUS_COMMITTED);
+                return;
             }
-            return;
+            // try to enlist our listener
+            isEnlisted.set(registerSynchronization(this));
         }
-
-        DocumentEventContext docCtx;
-        if (event.getContext() instanceof DocumentEventContext) {
-            docCtx = (DocumentEventContext) event.getContext();
-        } else {
+        if (!(event.getContext() instanceof DocumentEventContext)) {
             // don't process Events that are not tied to Documents
             return;
         }
+        DocumentEventContext docCtx = (DocumentEventContext) event.getContext();
         stackCommand(docCtx, eventId);
-
-        // register via Tx Manager
-        if (!synched.get() && useTxSync) {
-            synched.set(registerSynchronization(this));
-        }
-
     }
 
     protected void stackCommand(DocumentEventContext docCtx, String eventId) {
-
         if (Framework.isTestModeSet() && Framework.getRuntime().isShuttingDown()) {
-            // avoid processing events generated by the shutdown process
+            // avoid processing events generated by the shutdown process in test framework
             return;
         }
-
         DocumentModel doc = docCtx.getSourceDocument();
-
         if (doc == null) {
             return;
         }
-
         Boolean block = (Boolean) docCtx.getProperty(EventConstants.DISABLE_AUTO_INDEXING);
         if (block != null && block) {
-            // ignore the event - we are blocked by the caller
             if (log.isDebugEnabled()) {
-                log.debug("Skip indexing for doc " + docCtx.getSourceDocument());
+                log.debug("Indexing is disable, skip indexing command for doc " + doc);
             }
             return;
         }
-
-        Boolean sync = (Boolean) docCtx.getProperty(EventConstants.ES_SYNC_INDEXING_FLAG);
-        if (sync == null) {
-            sync = (Boolean) doc.getContextData().get(EventConstants.ES_SYNC_INDEXING_FLAG);
-        }
-
-        if (sync == null) {
-            sync = useSyncIndexing.get();
-            if (sync == null) {
-                sync = false;
-            }
-        }
-
+        boolean sync = isSynchronous(docCtx, doc);
         stackCommand(doc, eventId, sync);
     }
 
-    @Override
-    protected void fireSyncIndexing(List<IndexingCommand> syncCommands) throws ClientException {
-        ElasticSearchIndexing esi = Framework.getLocalService(ElasticSearchIndexing.class);
-        for (IndexingCommand cmd : syncCommands) {
-            esi.scheduleIndexing(cmd);
+    protected boolean isSynchronous(DocumentEventContext docCtx, DocumentModel doc) {
+        // 1. look at event context
+        Boolean sync = (Boolean) docCtx.getProperty(EventConstants.ES_SYNC_INDEXING_FLAG);
+        if (sync != null) {
+            return sync;
         }
-    }
-
-    @Override
-    protected void fireAsyncIndexing(List<IndexingCommand> asyncCommands) throws ClientException {
-        ElasticSearchIndexing esi = Framework.getLocalService(ElasticSearchIndexing.class);
-        for (IndexingCommand cmd : asyncCommands) {
-            esi.scheduleIndexing(cmd);
+        // 2. look at document context
+        sync = (Boolean) doc.getContextData().get(EventConstants.ES_SYNC_INDEXING_FLAG);
+        if (sync != null) {
+            return sync;
         }
+        // 3. look at Local thread flag
+        sync = useSyncIndexing.get();
+        if (sync != null) {
+            return sync;
+        }
+        return false;
     }
 
     @Override
     public void beforeCompletion() {
-        try {
-            // before completion is not called if the tx is marked as rollback
-            flushCommands();
-        } catch (ClientException e) {
-            log.error("Error during flush", e);
-        } finally {
-            synched.set(false);
-        }
+
     }
 
     @Override
     public void afterCompletion(int status) {
-        if (Status.STATUS_MARKED_ROLLBACK == status || Status.STATUS_ROLLEDBACK == status) {
-            synched.set(false);
+        try {
+            if (Status.STATUS_MARKED_ROLLBACK == status || Status.STATUS_ROLLEDBACK == status) {
+                return;
+            }
+            List<IndexingCommand> commandList = new ArrayList<>();
+            for (IndexingCommands cmds : getAllCommands().values()) {
+                for (IndexingCommand cmd : cmds.getCommands()) {
+                    commandList.add(cmd);
+                }
+            }
+            ElasticSearchIndexing esi = Framework.getLocalService(ElasticSearchIndexing.class);
+            esi.index(commandList);
+        } finally {
+            isEnlisted.set(false);
             getAllCommands().clear();
+            useSyncIndexing.set(null);
         }
-        useSyncIndexing.set(null);
     }
 
     public static ThreadLocal<Boolean> useSyncIndexing = new ThreadLocal<Boolean>() {

codenvy-demos/contrib-nuxeo

nuxeo-features/nuxeo-elasticsearch/nuxeo-elasticsearch-core/src/main/java/org/nuxeo/elasticsearch/listener/ElasticSearchInlineListener.java
@@ -17,6 +17,7 @@
 
 package org.nuxeo.elasticsearch.listener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -46,17 +47,14 @@ public class ElasticSearchInlineListener extends IndexingCommandsStacker impleme
 
     private static final Log log = LogFactory.getLog(ElasticSearchInlineListener.class);
 
-    // rely on TransactionManager rather than on Save event
-    protected static boolean useTxSync = true;
-
     protected static ThreadLocal<Map<String, IndexingCommands>> transactionCommands = new ThreadLocal<Map<String, IndexingCommands>>() {
         @Override
         protected HashMap<String, IndexingCommands> initialValue() {
-            return new HashMap<String, IndexingCommands>();
+            return new HashMap<>();
         }
     };
 
-    protected static ThreadLocal<Boolean> synched = new ThreadLocal<Boolean>() {
+    protected static ThreadLocal<Boolean> isEnlisted = new ThreadLocal<Boolean>() {
         @Override
         protected Boolean initialValue() {
             return Boolean.FALSE;
@@ -70,105 +68,90 @@ public class ElasticSearchInlineListener extends IndexingCommandsStacker impleme
 
     @Override
     public void handleEvent(Event event) throws ClientException {
-
         String eventId = event.getName();
-
-        // manual flush on save if TxManager is not hooked
-        if (event.isCommitEvent()) {
-            if (!synched.get()) {
-                flushCommands();
+        if (!isEnlisted.get()) {
+            if (event.isCommitEvent()) {
+                // manual flush on save if TxManager is not hooked
+                afterCompletion(Status.STATUS_COMMITTED);
+                return;
             }
-            return;
+            // try to enlist our listener
+            isEnlisted.set(registerSynchronization(this));
         }
-
-        DocumentEventContext docCtx;
-        if (event.getContext() instanceof DocumentEventContext) {
-            docCtx = (DocumentEventContext) event.getContext();
-        } else {
+        if (!(event.getContext() instanceof DocumentEventContext)) {
             // don't process Events that are not tied to Documents
             return;
         }
+        DocumentEventContext docCtx = (DocumentEventContext) event.getContext();
         stackCommand(docCtx, eventId);
-
-        // register via Tx Manager
-        if (!synched.get() && useTxSync) {
-            synched.set(registerSynchronization(this));
-        }
-
     }
 
     protected void stackCommand(DocumentEventContext docCtx, String eventId) {
-
         if (Framework.isTestModeSet() && Framework.getRuntime().isShuttingDown()) {
-            // avoid processing events generated by the shutdown process
+            // avoid processing events generated by the shutdown process in test framework
             return;
         }
-
         DocumentModel doc = docCtx.getSourceDocument();
-
         if (doc == null) {
             return;
         }
-
         Boolean block = (Boolean) docCtx.getProperty(EventConstants.DISABLE_AUTO_INDEXING);
         if (block != null && block) {
-            // ignore the event - we are blocked by the caller
             if (log.isDebugEnabled()) {
-                log.debug("Skip indexing for doc " + docCtx.getSourceDocument());
+                log.debug("Indexing is disable, skip indexing command for doc " + doc);
             }
             return;
         }
-
-        Boolean sync = (Boolean) docCtx.getProperty(EventConstants.ES_SYNC_INDEXING_FLAG);
-        if (sync == null) {
-            sync = (Boolean) doc.getContextData().get(EventConstants.ES_SYNC_INDEXING_FLAG);
-        }
-
-        if (sync == null) {
-            sync = useSyncIndexing.get();
-            if (sync == null) {
-                sync = false;
-            }
-        }
-
+        boolean sync = isSynchronous(docCtx, doc);
         stackCommand(doc, eventId, sync);
     }
 
-    @Override
-    protected void fireSyncIndexing(List<IndexingCommand> syncCommands) throws ClientException {
-        ElasticSearchIndexing esi = Framework.getLocalService(ElasticSearchIndexing.class);
-        for (IndexingCommand cmd : syncCommands) {
-            esi.scheduleIndexing(cmd);
+    protected boolean isSynchronous(DocumentEventContext docCtx, DocumentModel doc) {
+        // 1. look at event context
+        Boolean sync = (Boolean) docCtx.getProperty(EventConstants.ES_SYNC_INDEXING_FLAG);
+        if (sync != null) {
+            return sync;
         }
-    }
-
-    @Override
-    protected void fireAsyncIndexing(List<IndexingCommand> asyncCommands) throws ClientException {
-        ElasticSearchIndexing esi = Framework.getLocalService(ElasticSearchIndexing.class);
-        for (IndexingCommand cmd : asyncCommands) {
-            esi.scheduleIndexing(cmd);
+        // 2. look at document context
+        sync = (Boolean) doc.getContextData().get(EventConstants.ES_SYNC_INDEXING_FLAG);
+        if (sync != null) {
+            return sync;
         }
+        // 3. look at Local thread flag
+        sync = useSyncIndexing.get();
+        if (sync != null) {
+            return sync;
+        }
+        return false;
     }
 
     @Override
     public void beforeCompletion() {
-        try {
-            // before completion is not called if the tx is marked as rollback
-            flushCommands();
-        } catch (ClientException e) {
-            log.error("Error during flush", e);
-        } finally {
-            synched.set(false);
-        }
+
     }
 
     @Override
     public void afterCompletion(int status) {
-        if (Status.STATUS_MARKED_ROLLBACK == status || Status.STATUS_ROLLEDBACK == status) {
-            synched.set(false);
-            getAllCommands().clear();
+        if (getAllCommands().isEmpty()) {
+            return;
         }
-        useSyncIndexing.set(null);
+        try {
+            if (Status.STATUS_MARKED_ROLLBACK == status || Status.STATUS_ROLLEDBACK == status) {
+                return;
+            }
+            List<IndexingCommand> commandList = new ArrayList<>();
+            for (IndexingCommands cmds : getAllCommands().values()) {
+                for (IndexingCommand cmd : cmds.getCommands()) {
+                    commandList.add(cmd);
+                }
+            }
+            ElasticSearchIndexing esi = Framework.getLocalService(ElasticSearchIndexing.class);
+            esi.runIndexingWorker(commandList);
+        } finally {
+            isEnlisted.set(false);
+            getAllCommands().clear();
+            useSyncIndexing.set(null);
+        }
     }
 
     public static ThreadLocal<Boolean> useSyncIndexing = new ThreadLocal<Boolean>() {